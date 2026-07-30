e3686e693520c10adaa7c97c4699cb5a7438a756
Sthaneekam Amrutha Sree <sree247778@gmail.com>
2026-07-30 18:32:52 +0530

    Update database configuration for VERCEL support
    Modify database settings to use SQLite conditionally based on the VERCEL environment variable.

--- smartparcel/settings.py
+++ smartparcel/settings.py
@@ -108,18 +108,26 @@ WSGI_APPLICATION = "smartparcel.wsgi.application"
 # production if you swap in Postgres.
 
 
-import dj_database_url
+
 import os
+from pathlib import Path
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+if os.getenv('VERCEL'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/tmp/db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
